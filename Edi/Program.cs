f1a23881104742155580bd85fcb007faab148967
Łukasz Budziak <elotoja@protonmail.com>
2024-04-15 10:06:31 +0200

    refactor: Refactor Program.cs for better structure and readability (#4)
    - Move Main method to be static in Program class
    - Refactor CheckParse and DownloadMessages methods for improved code organization
    - Update code formatting and indentation for consistency
    
    Co-authored-by: elotoja <lukasz.budziak@masters.com.pl>

--- Edi/Program.cs
+++ Edi/Program.cs
@@ -4,47 +4,46 @@ using Edi.Generator;
 using indice.Edi;
 using System.Text.Json;
 
-namespace Edi
+namespace Edi;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        CheckParse();
+        //GenerateCode();
+        //DownloadMessages();
+    }
+
+    private static void DownloadMessages()
+    {
+        var downloader = new Downloader();
+        downloader.Download();
+        //downloader.DownloadMessageSpecification("D97A", "INVOIC");
+    }
+
+    private static void CheckParse()
+    {
+        string dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edi");
+        string filePath = Path.Combine(dirPath, "INVOIC_a2i24021713091266429e1.edi.c2e");
+
+        var grammar = EdiGrammar.NewEdiFact();
+        var interchange = default(Interchange<Contracts.Messages.INVOIC>);
+        using var stream = new StreamReader(filePath);
+        interchange = new EdiSerializer().Deserialize<Interchange<Contracts.Messages.INVOIC>>(stream, grammar);
+
+        string json = JsonSerializer.Serialize(interchange, new JsonSerializerOptions
         {
-            CheckParse();
-            //GenerateCode();
-            //DownloadMessages();
-        }
+            WriteIndented = true,
+        });
 
-        private static void DownloadMessages()
-        {
-            var downloader = new Downloader();
-            downloader.Download();
-            //downloader.DownloadMessageSpecification("D97A", "INVOIC");
-        }
+        File.WriteAllText(Path.Combine(dirPath, "out.json"), json);
+    }
 
-        private static void CheckParse()
-        {
-            string dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edi");
-            string filePath = Path.Combine(dirPath, "INVOIC_a2i24021713091266429e1.edi.c2e");
+    private static void GenerateCode()
+    {
+        var generator = new GeneratorService();
 
-            var grammar = EdiGrammar.NewEdiFact();
-            var interchange = default(Interchange<Contracts.Messages.INVOIC>);
-            using var stream = new StreamReader(filePath);
-            interchange = new EdiSerializer().Deserialize<Interchange<Contracts.Messages.INVOIC>>(stream, grammar);
-
-            string json = JsonSerializer.Serialize(interchange, new JsonSerializerOptions
-            {
-                WriteIndented = true,
-            });
-
-            File.WriteAllText(Path.Combine(dirPath, "out.json"), json);
-        }
-
-        private static void GenerateCode()
-        {
-            var generator = new GeneratorService();
-
-            generator.Generate();
-        }
+        generator.Generate();
     }
 }

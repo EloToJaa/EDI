378de9b7b4d5fd65bdfd0356ae909a94a482be49
elotoja <lukasz.budziak@masters.com.pl>
2024-04-18 13:17:23 +0200

    refactor: Refactor CheckParse method in Program.cs
    - Updated CheckParse method to deserialize interchange based on messageName
    - Refactored InterchangeFactory to create interchange types based on messageClassName

---
+++ Edi/InterchangeFactory.cs
@@ -0,0 +1,46 @@
+﻿using Edi.Contracts.Interchanges;
+using Edi.Contracts.InterchangeSegments;
+using Edi.Contracts.Interfaces;
+using Edi.Contracts.Messages;
+using System.Reflection;
+
+namespace Edi;
+
+public class InterchangeFactory
+{
+    public static Interchange<IMessage>? Create(string messageClassName)
+    {
+        var asm = Assembly.Load("Edi.Contracts, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+        var messageType = asm.GetType($"Edi.Contracts.Messages.{messageClassName}");
+        if (messageType is null) return null;
+
+        if (!typeof(IMessage).IsAssignableFrom(messageType))
+            return null;
+
+        IMessage? message = (IMessage?)Activator.CreateInstance(messageType);
+
+        if (message is null) return null;
+
+        return new Interchange<IMessage>
+        {
+            InterchangeHeader = new UNB(),
+            Message = message,
+            InterchangeTrailer = new UNZ()
+        };
+    }
+
+    public static Type? CreateType(string messageClassName)
+    {
+        var asm = Assembly.Load("Edi.Contracts, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+        var messageType = asm.GetType($"Edi.Contracts.Messages.{messageClassName}");
+        if (messageType is null) return null;
+
+        if (!typeof(IMessage).IsAssignableFrom(messageType))
+            return null;
+
+        var interchangeGenericType = typeof(Interchange<>);
+        var interchangeType = interchangeGenericType.MakeGenericType(messageType);
+
+        return interchangeType;
+    }
+}
--- Edi/Program.cs
+++ Edi/Program.cs
@@ -1,8 +1,10 @@
-﻿using Edi.Contracts.Interchanges;
-using Edi.Download;
+﻿using Edi.Download;
 using Edi.Generator;
 using indice.Edi;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace Edi;
 
@@ -11,7 +13,7 @@ internal class Program
     static void Main(string[] args)
     {
         CheckParse();
-        GenerateCode();
+        //GenerateCode();
         //DownloadMessages();
     }
 
@@ -24,20 +26,39 @@ internal class Program
 
     private static void CheckParse()
     {
-        //string dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edi");
-        //string filePath = Path.Combine(dirPath, "INVOIC_a2i24021713091266429e1.edi.c2e");
+        string dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edi");
+        string filePath = Path.Combine(dirPath, "ORDERS_2024_281.c2e (1).sent");
+        string fileContents = File.ReadAllText(filePath);
 
-        //var grammar = EdiGrammar.NewEdiFact();
-        //var interchange = default(Interchange<Contracts.Messages.INVOIC>);
-        //using var stream = new StreamReader(filePath);
-        //interchange = new EdiSerializer().Deserialize<Interchange<Contracts.Messages.INVOIC>>(stream, grammar);
+        var segments = fileContents.Split("'").Select(s => s.Trim());
+        string unhSegment = segments.Where(s => s.StartsWith("UNH")).First();
+        var elements = unhSegment.Split("+");
+        string versionElement = elements[2];
+        var values = versionElement.Split(":");
 
-        //string json = JsonSerializer.Serialize(interchange, new JsonSerializerOptions
-        //{
-        //    WriteIndented = true,
-        //});
+        string messageName = values[0];
+        string versionNumber = values[1] + values[2];
 
-        //File.WriteAllText(Path.Combine(dirPath, "out.json"), json);
+        //object? interchange = InterchangeFactory.Create(messageName);
+        //var interchange = default(Interchange<ORDERS>);
+
+        var type = InterchangeFactory.CreateType(messageName);
+
+        if (type is null) return;
+
+        var grammar = EdiGrammar.NewEdiFact();
+        using var stream = new StreamReader(filePath);
+
+        //interchange = new EdiSerializer().Deserialize<Interchange<ORDERS>>(stream, grammar);
+        object? interchange = new EdiSerializer().Deserialize(stream, grammar, type);
+
+        string json = JsonSerializer.Serialize(interchange, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        });
+
+        File.WriteAllText(Path.Combine(dirPath, "out.json"), Regex.Unescape(json));
     }
 
     private static void GenerateCode()
